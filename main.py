9a18aeeca4b45e094f64a7bd3232e1de108da710
lilin <lilin@myrual.me>
2019-04-05 20:09:35 +0800

    open csv file and confirm wallet select

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import *
 
 import time
 import traceback, sys
+import wallet_api
 
 
 class WorkerSignals(QObject):
@@ -36,12 +37,22 @@ class Worker(QRunnable):
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        button_action = QAction("Open wallet file", self)
+        button_action.setStatusTip("Open wallet file")
+        button_action.triggered.connect(self.open_file)
+ 
+        menu = self.menuBar()
+        file_menu = menu.addMenu("File")
+        file_menu.addAction(button_action)
+
 
         self.counter = 0
         layout = QVBoxLayout()
-        self.l = QLabel("Start")
+        self.rootLayout = layout
+        self.l = QLabel("Need load wallet")
+        self.l.setAlignment(Qt.AlignCenter)
 
-        b = QPushButton("DANGER!")
+        b = QPushButton("Open Wallet file")
         b.pressed.connect(self.open_file)
         layout.addWidget(self.l)
         layout.addWidget(b)
@@ -67,9 +78,26 @@ class MainWindow(QMainWindow):
     def thread_complete(self):
         print("THREAD COMPLETE")
     def open_file(self):
-        file_name = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","All Files (*);;CSV Files (*.csv)")
-        print("selected_file:")
-        print(file_name)
+        file_name_selected = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","All Files (*);;CSV Files (*.csv)")
+        file_name = file_name_selected[0]
+        fileter   = file_name_selected[1]
+        if(file_name == ''):
+            print("User cancel file")
+        else:
+            print("User select file with %s" % file_name)
+            self.l.setText("Select Wallet record")
+            wallet_records = wallet_api.load_wallet_csv_file(file_name)
+            wallet_list = QListWidget()
+            i = 0
+            for each_wallet in wallet_records:
+                this_list_item = QListWidgetItem()
+                this_list_item.setData(0x0100, each_wallet)
+                this_list_item.setText(each_wallet.userid)
+                wallet_list.addItem(this_list_item)
+                i+=1
+            wallet_list.itemClicked.connect(self.wallet_list_record_selected)
+            self.rootLayout.addWidget(wallet_list)
+
     def oh_no(self):
         worker = Worker(self.execute_this_fn)
         worker.signals.result.connect(self.print_output)
@@ -77,7 +105,10 @@ class MainWindow(QMainWindow):
         self.threadPool.start(worker)
     def recurring_timer(self):
         self.counter += 1
-        self.l.setText("Counter %d" % self.counter)
+    def wallet_list_record_selected(self, itemSelect):
+        data_in_item = itemSelect.data(0x0100)
+        print(data_in_item)
+        print("%s selected"%data_in_item.userid)
 
 app = QApplication([])
 window = MainWindow()
