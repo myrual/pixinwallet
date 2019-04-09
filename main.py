7c3271ff9e043be8b51904ecb4f40e47f122e5b3
lilin <lilin@myrual.me>
2019-04-09 19:34:29 +0800

    create asset address in gui

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ class WorkerSignals(QObject):
     error = pyqtSignal(tuple)
     result = pyqtSignal(object)
     progress = pyqtSignal(object)
-class Balance_Thread(QRunnable):
-    def __init__(self, wallt_obj, *args, **kwargs):
-        super(Balance_Thread, self).__init__()
-        self.wallt_obj = wallt_obj
+class Asset_addresses_Thread(QRunnable):
+    def __init__(self, wallet_obj, asset_obj, *args, **kwargs):
+        super(Asset_addresses_Thread, self).__init__()
+        self.wallet_obj = wallet_obj
+        self.asset_obj  = asset_obj
         self.args = args
         self.kwargs = kwargs
         self.signals = WorkerSignals()
@@ -24,7 +25,30 @@ class Balance_Thread(QRunnable):
     @pyqtSlot()
     def run(self):
         try:
-            result = self.wallt_obj.get_balance()
+            result = self.wallet_obj.get_asset_withdrawl_addresses(self.asset_obj.asset_id)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
+        print("Withdraw address of asset Thread")
+
+
+class Balance_Thread(QRunnable):
+    def __init__(self, wallet_obj, *args, **kwargs):
+        super(Balance_Thread, self).__init__()
+        self.wallet_obj = wallet_obj
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        try:
+            result = self.wallet_obj.get_balance()
         except:
             traceback.print_exc()
             exctype, value = sys.exc_info()[:2]
@@ -106,12 +130,9 @@ class MainWindow(QMainWindow):
         self.counter = 0
         layout = QVBoxLayout()
         self.rootLayout = layout
-        self.l = QLabel("Need load wallet")
-        self.l.setAlignment(Qt.AlignCenter)
 
         b = QPushButton("Open Wallet file")
         b.pressed.connect(self.open_file)
-        layout.addWidget(self.l)
         layout.addWidget(b)
         new_wallet_btn = QPushButton("Create Wallet file")
         new_wallet_btn.pressed.connect(self.pop_create_wallet_window)
@@ -183,7 +204,6 @@ class MainWindow(QMainWindow):
             print("User cancel file")
         else:
             print("User select file with %s" % file_name)
-            self.l.setText("Select Wallet record")
             self.selected_wallet_record = wallet_api.load_wallet_from_clear_base64_file(file_name)
             self.open_selected_wallet()
     def select_file_for_create_wallet(self):
@@ -217,6 +237,51 @@ class MainWindow(QMainWindow):
         self.threadPool.start(worker)
         self.go_create_accout_btn.setDisabled(True)
         
+
+    def close_Create_Windows(self):
+        self.create_withdraw_address_widget.close()
+        return
+    def pressed_create_withdraw_address_bitcoin(self):
+        print("Account tag %s, deposit address %s, pin %s"%(self.account_tag_edit.text(), self.public_key_edit.text(), self.asset_pin_edit.text()))
+        self.Add_address_btn.setDisabled(True)
+        create_address_result  = self.selected_wallet_record.create_address(self.asset_instance_in_item.asset_id, self.public_key_edit.text(), self.account_tag_edit.text(), asset_pin = self.asset_pin_edit.text())
+        if create_address_result.is_success:
+            print("Address is created with id %s"%create_address_result.data.address_id)
+            OK_button = QPushButton("Done")
+            OK_button.pressed.connect(self.close_Create_Windows)
+            self.create_withdraw_address_layout.addWidget(OK_button)
+        else:
+            print("Failed to create address ")
+
+    def pop_create_withdraw_address_window_bitcoinstyle(self):
+
+
+        account_tag_widget    = QLabel("Account alias:")
+        self.account_tag_edit = QLineEdit()
+        public_key_widget     = QLabel("Deposit address:")
+        self.public_key_edit  = QLineEdit()
+        asset_pin_widget     = QLabel("Asset pin:")
+        self.asset_pin_edit  = QLineEdit()
+        self.asset_pin_edit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+        Add_address_btn       = QPushButton("Create")
+        Add_address_btn.pressed.connect(self.pressed_create_withdraw_address_bitcoin)
+        self.Add_address_btn = Add_address_btn
+
+        create_withdraw_address_layout = QVBoxLayout()
+        create_withdraw_address_layout.addWidget(account_tag_widget)
+        create_withdraw_address_layout.addWidget(self.account_tag_edit)
+        create_withdraw_address_layout.addWidget(public_key_widget)
+        create_withdraw_address_layout.addWidget(self.public_key_edit)
+        create_withdraw_address_layout.addWidget(asset_pin_widget)
+        create_withdraw_address_layout.addWidget(self.asset_pin_edit)
+        create_withdraw_address_layout.addWidget(Add_address_btn)
+
+        self.create_withdraw_address_layout = create_withdraw_address_layout
+
+        self.create_withdraw_address_widget = QWidget()
+        self.create_withdraw_address_widget.setLayout(create_withdraw_address_layout)
+        self.create_withdraw_address_widget.show()
+
 
     def pop_create_wallet_window(self):
 
@@ -272,7 +337,6 @@ class MainWindow(QMainWindow):
             print("User cancel file")
         else:
             print("User select file with %s" % file_name)
-            self.l.setText("Select Wallet record")
             self.selected_wallet_record = wallet_api.load_wallet_from_clear_base64_file(file_name)
             self.open_selected_wallet()
 
@@ -284,6 +348,23 @@ class MainWindow(QMainWindow):
         self.threadPool.start(worker)
     def recurring_timer(self):
         self.counter += 1
+    def received_asset_withdraw_addresses_result(self, withdraw_addresses_asset_result):
+        if(withdraw_addresses_asset_result.is_success):
+            withdraw_address_list = QListWidget()
+            for eachAsset in withdraw_addresses_asset_result.data:
+                this_list_item = QListWidgetItem()
+                this_list_item.setData(0x0100, eachAsset)
+                this_list_item.setText(eachAsset.label)
+                withdraw_address_list.addItem(this_list_item)
+            withdraw_address_list.itemClicked.connect(self.withdrawaddress_list_record_selected)
+            withdraw_address_list.currentItemChanged.connect(self.withdraw_address_list_record_selection_actived)
+            self.withdraw_addresses_list_widget = withdraw_address_list
+
+            self.withdraw_addresses_list_and_new_layout.insertWidget(0, self.withdraw_addresses_list_widget)
+            if(len(withdraw_addresses_asset_result.data) > 0):
+                self.withdraw_addresses_list_widget.setCurrentRow(0)
+        return
+
     def received_balance_result(self, balance_result):
         if(balance_result.is_success):
             balance_list = QListWidget()
@@ -307,10 +388,80 @@ class MainWindow(QMainWindow):
         for each_seg in deposit_address_title_value_segments:
             deposit_label_content += each_seg["title"] + " : " + each_seg["value"] + "\n"
         self.asset_deposit_label.setText(deposit_label_content)
+    def withdraw_address_list_record_selection_actived(self,itemCurr, itemPre):
+        self.withdraw_address_instance_in_item = itemCurr.data(0x0100)
+        self.update_asset_address_detail(self.withdraw_address_instance_in_item)
+
 
     def balance_list_record_selected(self, itemSelect):
         self.asset_instance_in_item = itemSelect.data(0x0100)
         self.asset_balance_label.setText(self.asset_instance_in_item.balance)
+    def update_asset_address_detail(self, this_withdraw_address):
+        self.public_key_label_withdraw_address_asset.setText(this_withdraw_address.public_key)
+        self.label_label_withdraw_address_asset.setText(this_withdraw_address.label)
+        self.account_name_label_withdraw_address_asset.setText(this_withdraw_address.account_name)
+        self.account_tag_label_withdraw_address_asset.setText(this_withdraw_address.account_tag)
+        self.fee_label_withdraw_address_asset.setText(this_withdraw_address.fee)
+        self.reserve_label_withdraw_address_asset.setText(this_withdraw_address.reserve)
+        self.dust_label_withdraw_address_asset.setText(this_withdraw_address.dust)
+        self.updated_at_label_withdraw_address_asset.setText(this_withdraw_address.updated_at) 
+
+    def withdrawaddress_list_record_selected(self, itemSelect):
+        self.withdraw_address_instance_in_item = itemSelect.data(0x0100)
+        self.update_asset_address_detail(self.withdraw_address_instance_in_item)
+
+
+
+
+    def open_widget_manage_asset(self):
+        if (hasattr(self, "asset_instance_in_item")):
+            
+            add_withdraw_address_asset_btn = QPushButton("Add new address")
+            add_withdraw_address_asset_btn.pressed.connect(self.pop_create_withdraw_address_window_bitcoinstyle)
+
+            self.withdraw_addresses_list_and_new_layout = QVBoxLayout()
+            self.withdraw_addresses_list_and_new_layout.addWidget(add_withdraw_address_asset_btn)
+
+            withdraw_addresses_list_and_new_widget = QWidget()
+            withdraw_addresses_list_and_new_widget.setLayout(self.withdraw_addresses_list_and_new_layout)
+
+            self.public_key_label_withdraw_address_asset = QLabel("public key")
+            self.label_label_withdraw_address_asset = QLabel("label")
+            self.account_name_label_withdraw_address_asset = QLabel("account name:")
+            self.account_tag_label_withdraw_address_asset = QLabel("account label:")
+            self.fee_label_withdraw_address_asset = QLabel("fee")
+            self.reserve_label_withdraw_address_asset = QLabel("reserve")
+            self.dust_label_withdraw_address_asset = QLabel("dust")
+            self.updated_at_label_withdraw_address_asset = QLabel("updated at")
+            remove_address_btn = QPushButton("Delete")
+
+            withdraw_addresses_detail_layout = QVBoxLayout()
+
+            withdraw_addresses_detail_layout.addWidget(self.public_key_label_withdraw_address_asset)
+            withdraw_addresses_detail_layout.addWidget(self.label_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.account_name_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.account_tag_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.fee_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.reserve_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.dust_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(self.updated_at_label_withdraw_address_asset )
+            withdraw_addresses_detail_layout.addWidget(remove_address_btn)
+
+            withdraw_addresses_detail_widget = QWidget()
+            withdraw_addresses_detail_widget.setLayout(withdraw_addresses_detail_layout)
+            withdraw_addresses_list_and_new_detail_layout = QHBoxLayout()
+            withdraw_addresses_list_and_new_detail_layout.addWidget(withdraw_addresses_list_and_new_widget)
+            withdraw_addresses_list_and_new_detail_layout.addWidget(withdraw_addresses_detail_widget)
+            self.withdraw_addresses_list_and_new_detail_Widget = QWidget()
+            self.withdraw_addresses_list_and_new_detail_Widget.setLayout(withdraw_addresses_list_and_new_detail_layout)
+            self.withdraw_addresses_list_and_new_detail_Widget.show()
+
+            worker = Asset_addresses_Thread(self.selected_wallet_record, self.asset_instance_in_item)
+            worker.signals.result.connect(self.received_asset_withdraw_addresses_result)
+            worker.signals.finished.connect(self.thread_complete)
+            self.threadPool.start(worker)
+        else:
+            return
 
 
     def open_selected_wallet(self):
@@ -318,7 +469,8 @@ class MainWindow(QMainWindow):
             self.balance_and_detail_layout = QHBoxLayout()
             self.Balance_detail_layout = QVBoxLayout()
             self.selected_asset_send = QPushButton("Send")
-            self.selected_asset_manageasset = QPushButton("Manage address")
+            self.selected_asset_manageasset = QPushButton("Manage contact")
+            self.selected_asset_manageasset.pressed.connect(self.open_widget_manage_asset)
 
             self.asset_balance_label = QLabel()
             balance_font = self.asset_balance_label.font()
